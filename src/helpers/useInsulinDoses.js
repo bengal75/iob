5cd11df92495d0939e1a44fd56d329d51045fd57
Ben Galloway <git@bengalloway.io>
2019-09-10 02:22:21 +0100

    Build out functionality of Doses page

--- src/helpers/useInsulinDoses.js
+++ src/helpers/useInsulinDoses.js
@@ -1,16 +1,34 @@
 import createPersistedState from "use-persisted-state";
+import useInsulinParams from "./useInsulinParameters";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
+import addMinutes from "date-fns/addMinutes";
+import isPast from "date-fns/isPast";
 
 const useDoseState = createPersistedState("insulinDoses");
 
 const useInsulinDoses = () => {
   const [insulinDoses, setInsulinDoses] = useDoseState([]);
+  const { insulinParams } = useInsulinParams();
+
+  const parseTimestamp = timestamp => parse(timestamp, "t", new Date());
 
   const parsedDoses = insulinDoses.map(dose => ({
     units: Number(dose.units),
-    timestamp: parse(dose.timestamp, "t", new Date()),
+    timestamp: parseTimestamp(dose.timestamp),
   }));
+
+  const doseHasExpired = dose => {
+    const doseTimestamp =
+      dose.timestamp instanceof Date
+        ? dose.timestamp
+        : parseTimestamp(dose.timestamp);
+    const doseEndTime = addMinutes(
+      doseTimestamp,
+      insulinParams.durationOfInsulinActivity * 60
+    );
+    return isPast(doseEndTime);
+  };
 
   return {
     insulinDoses: parsedDoses,
@@ -25,6 +43,9 @@ const useInsulinDoses = () => {
       setInsulinDoses(
         insulinDoses.filter((value, index) => index !== removalIndex)
       ),
+    removeAllDepletedDoses: () =>
+      setInsulinDoses(insulinDoses.filter(dose => !doseHasExpired(dose))),
+    doseHasExpired,
   };
 };
 
--- src/routes/DosesPage.js
+++ src/routes/DosesPage.js
@@ -1,17 +1,124 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import PharmacyIcon from "@material-ui/icons/LocalPharmacy";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import CloseIcon from "@material-ui/icons/Close";
+import useInsulinDoses from "../helpers/useInsulinDoses";
 import { makeStyles } from "@material-ui/core/styles";
+import format from "date-fns/format";
 
 const useStyles = makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
 }));
 
+const sortReverseOnTimestamp = (doseA, doseB) => {
+  if (doseA.timestamp > doseB.timestamp) return -1;
+  if (doseA.timestamp < doseB.timestamp) return 1;
+  return 0;
+};
+
+const pluralise = (number, word) => (number === 1 ? word : `${word}s`);
+
+const SingleDose = ({ dose, deleteDose }) => {
+  const doseTimestamp = format(dose.timestamp, "HH:mm, EEE do MMM yyyy");
+  return (
+    <ListItem disabled={dose.expired}>
+      <ListItemIcon>
+        <PharmacyIcon />
+      </ListItemIcon>
+      <ListItemText
+        primary={`${dose.units} ${pluralise(dose.units, "unit")}`}
+      />
+      <ListItemIcon>
+        <AccessTimeIcon />
+      </ListItemIcon>
+      <ListItemText primary={doseTimestamp} />
+      <ListItemSecondaryAction onClick={() => deleteDose(dose.originalIndex)}>
+        <IconButton edge="end" aria-label="delete">
+          <CloseIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
+
+const DoseList = ({ type, doses, deleteDose, deleteAll }) => {
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+      >
+        <h2>{type} Doses</h2>
+        {type === "Depleted" && (
+          <Button variant="outlined" color="primary" onClick={deleteAll}>
+            Delete All
+          </Button>
+        )}
+      </Grid>
+      <List>
+        {doses.map(dose => (
+          <SingleDose
+            key={dose.timestamp}
+            dose={dose}
+            deleteDose={deleteDose}
+          />
+        ))}
+      </List>
+    </>
+  );
+};
+
 const DosesPage = () => {
   const classes = useStyles();
+  const {
+    insulinDoses,
+    removeInsulinDose,
+    removeAllDepletedDoses,
+    doseHasExpired,
+  } = useInsulinDoses();
+
+  const reverseChronoDoses = insulinDoses
+    .map((dose, idx) => ({
+      ...dose,
+      originalIndex: idx,
+      expired: doseHasExpired(dose),
+    }))
+    .sort(sortReverseOnTimestamp);
+  const activeDoses = reverseChronoDoses.filter(dose => !dose.expired);
+  const depletedDoses = reverseChronoDoses.filter(dose => dose.expired);
+
   return (
     <Grid>
       <div className={classes.toolbar} />
       <h1>Doses</h1>
+      {activeDoses.length !== 0 && (
+        <DoseList
+          type="Active"
+          doses={activeDoses}
+          deleteDose={removeInsulinDose}
+        />
+      )}
+      {depletedDoses.length !== 0 && (
+        <DoseList
+          type="Depleted"
+          doses={depletedDoses}
+          deleteDose={removeInsulinDose}
+          deleteAll={removeAllDepletedDoses}
+        />
+      )}
+      {!reverseChronoDoses.length && (
+        <p>There are no insulin doses recorded. Add some from the home page.</p>
+      )}
     </Grid>
   );
 };
