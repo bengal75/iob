245619ded33822e391a2e199d86551c0a7673ec1
Ben Galloway <git@bengalloway.io>
2019-09-10 02:22:42 +0100

    Visual styling for Settings page

--- src/routes/SettingsPage.js
+++ src/routes/SettingsPage.js
@@ -1,5 +1,11 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import TimelapseIcon from "@material-ui/icons/Timelapse";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import { makeStyles } from "@material-ui/core/styles";
 import useInsulinParams from "../helpers/useInsulinParameters";
 
@@ -14,7 +20,26 @@ const SettingsPage = () => {
     <Grid>
       <div className={classes.toolbar} />
       <h1>Settings</h1>
-      <pre>{JSON.stringify(insulinParams, null, 2)}</pre>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <TimelapseIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={`${insulinParams.durationOfInsulinActivity} hours`}
+            secondary="Duration of insulin activity"
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <TrendingUpIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={`${insulinParams.peak} minutes`}
+            secondary="Peak time of insulin activity"
+          />
+        </ListItem>
+      </List>
     </Grid>
   );
 };
